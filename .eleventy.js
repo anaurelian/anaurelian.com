51b98a228a7a0f1b88463467a8d9b98a2f03fe64
anaurelian <dev@anaurelian.com>
2021-10-25 23:17:06 +0300

    Add Hello World Counters

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@
 
 // const filters = require('./utils/filters.js');
 
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
+
 module.exports = function (eleventyConfig) {
 
   // Filters & Shortcodes
@@ -15,6 +18,9 @@ module.exports = function (eleventyConfig) {
 
   // Add support for YAML data files
   // eleventyConfig.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+  const markdownLib = markdownIt({ html: true }).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     htmlTemplateEngine: "njk",
---
+++ src/helloworldcounters/history/1.0/index.md
@@ -0,0 +1,8 @@
+---
+title: Version 1.0
+seoTitle:
+seoDescription:
+---
+
+_Released on Oct 25, 2021_  
+First public release
